384f1a47808ec63b9c6dfec411ec89e6858c5d85
Valder Gallo <valdergallo@gmail.com>
2016-10-29 13:52:47 -0200

    hum

--- pyconst/slug.py
+++ pyconst/slug.py
@@ -6,5 +6,5 @@ import re
 def slugify(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
     only_ascii = nfkd_form.encode('ASCII', 'ignore').lower()
-    only_string = re.findall(b'\w+', only_ascii)
+    only_string = re.findall(b'\w+', only_ascii.decode())
     return '_'.join(only_string)
