b998ce5cdd6095f7be891b5926374e6d7436178c
Valder Gallo <valdergallo@gmail.com>
2016-10-29 15:01:31 -0200

    fix suport for py3 and py2

--- pyconst/slug.py
+++ pyconst/slug.py
@@ -5,6 +5,6 @@ import re
 
 def slugify(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
-    only_ascii = nfkd_form.encode('ASCII', 'ignore').lower()
-    only_string = re.findall('\w+', str(only_ascii))
+    only_ascii = nfkd_form.encode('ASCII', 'ignore').decode('utf-8').lower()
+    only_string = re.findall('\w+', only_ascii)
     return '_'.join(only_string)
