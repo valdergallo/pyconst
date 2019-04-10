2b25482243159a1c915726b65a80da62c79e8f67
Valder Gallo <valder.gallo@nkey.com>
2019-04-10 13:38:44 -0300

    refac(pyconst) rename label to name ... as in enum

--- pyconst/const.py
+++ pyconst/const.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals, absolute_import
 from .slug import slugify as s
 from .slug import slugify_attr as s_attr
-
+import enum
 
 class PyConstString(str):
 
-    def __new__(cls, label=None, value=None, to_case='upper'):
+    def __new__(cls, name=None, value=None, to_case='upper'):
         if not value:
-            value = label
+            value = name
         if to_case == 'upper':
             value = s(value).upper()
         elif to_case == 'lower':
@@ -16,7 +16,7 @@ class PyConstString(str):
         else:
             value = s(value)
         obj = str.__new__(cls, value)
-        obj.label = label
+        obj.name = name
         obj.value = value
         return obj
 
@@ -28,49 +28,52 @@ class Const(object):
         for value in args:
             self.add(value)
 
-        for label, attr in kwargs.items():
-            self.add(label, attr)
+        for name, attr in kwargs.items():
+            self.add(name, attr)
 
     def __set_iter_value(self, iter_value):
-        label, attr, value = (None,) * 3
+        name, attr, value = (None,) * 3
         if len(iter_value) == 1:
-            label = iter_value[0]
+            name = iter_value[0]
         elif len(iter_value) == 2:
-            label, attr = iter_value
+            name, attr = iter_value
         elif len(iter_value) == 3:
-            label, attr, value = iter_value
+            name, attr, value = iter_value
         elif len(iter_value) > 3:
             attr = iter_value[1]
             value = iter_value[2]
-            label = iter_value[0]
-        return label, attr, value
+            name = iter_value[0]
+        return name, attr, value
 
-    def get_const_string(self, label, value):
-        return PyConstString(label=label, value=value, to_case="upper")
+    def get_const_string(self, name, value):
+        return PyConstString(name=name, value=value, to_case="upper")
 
-    def add(self, label, attr=None, value=None):
+    def to_enum(self):
+        return enum.Enum('DynamicEnum', {i[0]:i[0] for i in self})
+
+    def add(self, name, attr=None, value=None):
         "Set values in constant"
 
-        if isinstance(label, tuple) or isinstance(label, list):
-            label, attr, value = self.__set_iter_value(label)
+        if isinstance(name, tuple) or isinstance(name, list):
+            name, attr, value = self.__set_iter_value(name)
 
         if attr is None:
-            attr = label
+            attr = name
 
         if value is None:
             value = attr
 
-        self.__data += (self.get_const_string(label=label, value=value),)
+        self.__data += (self.get_const_string(name=name, value=value),)
         # set attribute as slugfiy
         self.__dict__[s_attr(attr)] = self.__data[-1]
 
     def __getitem__(self, index):
         "Get index item"
-        return (self.__data[index], self.__data[index].label)
+        return (self.__data[index], self.__data[index].name)
 
     def __iter__(self):
         "Lazy return"
-        return ((i, i.label) for i in self.__data)
+        return ((i, i.name) for i in self.__data)
 
     def __len__(self):
         return len(self.__data)
@@ -79,19 +82,19 @@ class Const(object):
 class LowerConst(Const):
     "Force value to lower case by default"
 
-    def get_const_string(self, label, value):
-        return PyConstString(label=label, value=value, to_case="lower")
+    def get_const_string(self, name, value):
+        return PyConstString(name=name, value=value, to_case="lower")
 
 
 class UpperConst(Const):
     "Force value to upper case"
 
-    def get_const_string(self, label, value):
-        return PyConstString(label=label, value=value, to_case="upper")
+    def get_const_string(self, name, value):
+        return PyConstString(name=name, value=value, to_case="upper")
 
 
 class DefaultConst(Const):
     "Default value with only using slugify"
 
-    def get_const_string(self, label, value):
-        return PyConstString(label=label, value=value, to_case=None)
+    def get_const_string(self, name, value):
+        return PyConstString(name=name, value=value, to_case=None)
